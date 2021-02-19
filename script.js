7dc6d74a5ffbe3b76aae77f831518dcd0b1432a1
Dave Powell <evadllewop@gmail.com>
2021-02-19 15:13:37 -0600

    added [H[2J functionality

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 
   function searchHistory(city) {
     const li = $("<li>").addClass("list-group-item list-group-item-action").text(city);
-    $(".history").append(li);
+    $(".cities").append(li);
   }
 
   // CURRENT WEATHER
@@ -21,9 +21,9 @@ $(document).ready(function () {
       })
       .then(function (data) {
 
-        if (history.indexOf(cityName) === -1) {
-          history.push(cityName);
-          window.localStorage.setItem("history", JSON.stringify(history));
+        if (cities.indexOf(cityName) === -1) {
+          cities.push(cityName);
+          window.localStorage.setItem("cities", JSON.stringify(cities));
           searchHistory(cityName);
         }
 
@@ -35,16 +35,37 @@ $(document).ready(function () {
         const humidityEl = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
         const tempEl = $("<p>").addClass("card-text").text("Temperature: " + tempF.toFixed(2) + " °F");
         const iconEl = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+
         const titleEl = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
-        const cardBody = $("<div>").addClass("card-body");
         const cardEl = $("<div>").addClass("card");
+        const cardBody = $("<div>").addClass("card-body");
+
 
         titleEl.append(iconEl);
-        cardBody.append(titleEl, tempEl, humidityEl, windEl);
         cardEl.append(cardBody);
+        cardBody.append(titleEl, tempEl, humidityEl, windEl);
         $("#current").append(cardEl);
 
+        $("#clear").empty();
+
+        const clearRow = $("<div>").addClass("row");
+        const clearDiv = $("<div>").addClass("col-md-8");
+        const clearBtn = $("<button>").text("Clear").addClass("btn btn-primary");
+
+        clearBtn.append(clearDiv);
+        clearDiv.append(clearRow);
+        $("#clear").append(clearBtn);
+        clearBtn.on("click", function () {
+          console.log("hello");
+          $(".cities").empty();
+          $("#clear").empty();
+          $("#current").empty();
+          $("#week").empty();
+          window.localStorage.clear();
+        });
+
         weekForecast(cityName);
+
       });
   };
 
@@ -60,24 +81,27 @@ $(document).ready(function () {
       .then(function (data) {
         $("#week").empty();
 
-        const weekTitle = $("<div>").html("<h4>5-Day Forecast:</h4>");
         const titleDiv = $("<div>").addClass("row");
+        const weekTitle = $("<div>").html("<h4>5-Day Forecast:</h4>");
         weekTitle.append(titleDiv);
         $("#week").append(weekTitle);
 
         for (var i = 0; i < data.list.length; i++) {
           if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
             const tempF = (data.list[i].main.temp_max - 273.15) * 1.80 + 32;
-            const titleEl = $("<h5>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+
             const iconEl = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
             const tempEl = $("<p>").addClass("card-text").text("Temp: " + tempF.toFixed(2) + " °F");
             const humidityEl = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
 
+            const titleEl = $("<h5>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
             const colEl = $("<div>").addClass("col-md-2");
             const cardEl = $("<div>").addClass("card bg-primary text-white");
             const cardBody = $("<div>").addClass("card-body p-2");
 
-            colEl.append(cardEl.append(cardBody.append(titleEl, iconEl, tempEl, humidityEl)));
+            colEl.append(cardEl);
+            cardEl.append(cardBody);
+            cardBody.append(titleEl, iconEl, tempEl, humidityEl);
             $("#week .row").append(colEl);
           }
         }
@@ -85,19 +109,19 @@ $(document).ready(function () {
   }
 
   //  HISTORY AND LOCAL STORAGE
-  $(".history").on("click", "li", function () {
+  $(".cities").on("click", "li", function () {
     currentWeather($(this).text());
   });
 
+  var cities = JSON.parse(window.localStorage.getItem("cities")) || [];
+
+  if (cities.length > 0) {
+    currentWeather(cities[cities.length - 1]);
 
 
-  var history = JSON.parse(window.localStorage.getItem("history")) || [];
-
-  if (history.length > 0) {
-    currentWeather(history[history.length - 1]);
   }
 
-  for (var i = 0; i < history.length; i++) {
-    searchHistory(history[i]);
+  for (var i = 0; i < cities.length; i++) {
+    searchHistory(cities[i]);
   }
 });
